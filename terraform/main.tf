4ca052d364284293c52e085113dcb4a223918b00
shastish <psureshece@gmail.com>
2023-04-18 12:09:11 +0530

    test1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
    region = "us-east-1"
-  access_key = "Access_key"
-  secret_key = "Secret_key"
 }
 #######################################################
 #Creating UAT VPC with CIDR: 172.16.0.0/16
