d0b160e2beb7f1ba1f7e68d270f431be142b5ae3
shastish <psureshece@gmail.com>
2023-04-18 12:06:11 +0530

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,38 +14,3 @@ resource "aws_vpc" "uat_vpc" {
 output "uat_vpcid" {
   value = aws_vpc.uat_vpc.id
 }
-#creating uat public subnets with cidr 172.16.0.0/24
-resource "aws_subnet" "uat_public_subnet" {
-  vpc_id                  = "${aws_vpc.uat_vpc.id}"
-  cidr_block              = "172.16.0.0/24"
-  availability_zone       = "ap-southeast-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name        = "test-public-subnet-1b"
-  }
-}
-output "public_subnetid" {
-  value = aws_subnet.uat_public_subnet.id
-}
-  #creating uat private subnets with cidr 172.16.1.0/24
-resource "aws_subnet" "uat_private_subnet" {
-  vpc_id                  = "${aws_vpc.uat_vpc.id}"
-  cidr_block              = "172.16.1.0/24"
-  availability_zone       = "ap-southeast-2b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name        = "test-private-subnet-1b"
-  }
-}
-output "private_subnetid" {
-  value = aws_subnet.uat_private_subnet.id
-}
-resource "aws_internet_gateway" "uat_igw" {
-    vpc_id                  = "${aws_vpc.uat_vpc.id}"
-  tags = {
-    "Name" = "test_igw"
-  }
-}
-output "internet_gateway_id" {
-    value = aws_internet_gateway.uat_igw.id
-}
